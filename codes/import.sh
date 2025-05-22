efb28fb0d361e0eb88bce089822811e4a3a67f2f
Mohammed Makbool <M.Makbool@outlook.sa>
2025-05-22 19:35:26 +0300

    added hbase export buckup and import scripts

---
+++ codes/import.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <HBase-table-name>"
+    exit 1
+fi
+
+tablename="$1"
+
+hbase org.apache.hadoop.hbase.mapreduce.Import "$tablename" /backup/"$tablename"
